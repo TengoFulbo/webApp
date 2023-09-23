4563c6265f11886668afeb494d3569c8cc9fab93
Joaquin Sellanes <135662269+JoaquinSellanes@users.noreply.github.com>
2023-09-23 02:57:54 -0300

    elemino comentario

--- src/js/navdropdown.js
+++ src/js/navdropdown.js
@@ -3,12 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const navList = document.querySelector('.navbar__list');
     const nav = document.querySelector('.navbar');
 
-    // Agregar un evento de clic al botón de menú
     toggleBtn.addEventListener('click', function() {
         navList.classList.toggle('show');
         nav.classList.toggle('showNav');
-
-        // Añadimos una clase después de un pequeño retraso para que la animación tenga tiempo para activarse
+        
         setTimeout(() => {
             nav.classList.toggle('show');
         }, 10);
