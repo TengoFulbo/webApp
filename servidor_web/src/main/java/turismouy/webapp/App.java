3c11a8476b9b6b8929d91449b023b8ed63d01a4e
Ezequiel <63671478+Dargon227@users.noreply.github.com>
2023-09-29 01:35:26 -0300

    bienvenida a nuestro servidor web
    muy básico hasta el moment (basic crud)

---
+++ servidor_web/src/main/java/turismouy/webapp/App.java
@@ -0,0 +1,18 @@
+package turismouy.webapp;
+// import svcentral.Usuario;
+// import svcentral.Usuario;
+import turismouy.svcentral.Fabrica;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        // Usuario user = new Usuario();
+        // user.mostrarInfo("Ezequiel");
+    }
+}
