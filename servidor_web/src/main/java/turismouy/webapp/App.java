bb36b15dcc241b9d8a37826e6b99eab5a4551c22
Ezequiel <63671478+Dargon227@users.noreply.github.com>
2023-09-30 00:23:52 -0300

    Conexión ✅
    Los proyectos se conectan de forma perfecta

--- servidor_web/src/main/java/turismouy/webapp/App.java
+++ servidor_web/src/main/java/turismouy/webapp/App.java
@@ -2,6 +2,9 @@ package turismouy.webapp;
 // import svcentral.Usuario;
 // import svcentral.Usuario;
 import turismouy.svcentral.Fabrica;
+import turismouy.svcentral.datatypes.dataUsuario;
+import turismouy.svcentral.interfaces.IUsuarioController;
+import turismouy.svcentral.utilidades.log;
 
 /**
  * Hello world!
@@ -12,6 +15,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Fabrica fabrica = Fabrica.getInstance();
+        IUsuarioController IUC = fabrica.getIUsuarioController();
+        try {
+            dataUsuario user = IUC.mostrarInfo("jorge123");
+            
+            log.info("Bienvenido a la webApp");
+            log.info("Nickname: " + user.getNickname() + " | Nombre: " + user.getNombre());
+        } catch (Exception e) {
+            log.error("Error:" + e.getMessage());
+        }
+        
         // Usuario user = new Usuario();
         // user.mostrarInfo("Ezequiel");
     }
