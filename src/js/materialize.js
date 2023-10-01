77332b0421f7d3b98ba8bb3806f7db045f623ed3
Joaquin Sellanes <135662269+JoaquinSellanes@users.noreply.github.com>
2023-10-01 17:24:27 -0300

    filter

--- src/js/materialize.js
+++ src/js/materialize.js
@@ -11,6 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems);
 });
