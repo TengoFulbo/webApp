7ef6c3a999c2fc26ec0fa4c1c5e4b9ac652adb08
Joaquin Sellanes <135662269+JoaquinSellanes@users.noreply.github.com>
2023-10-01 20:35:48 -0300

    ya me olvide cuantas cosass hice

--- src/js/materialize.js
+++ src/js/materialize.js
@@ -11,6 +11,48 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('#modalInscribirse');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('#modalConsulta');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('#modalNuevaSalida');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('#modalNuevaActividad');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var options = {
+      format: 'dd mmmm, yyyy', 
+      i18n: {
+          cancel: 'Cancelar',
+          clear: 'Limpiar',
+          done: 'Listo',
+          months: [
+              'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+              'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+          ],
+          monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+          weekdays: [
+              'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'
+          ],
+          weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+          weekdaysAbbrev: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+      }
+  };
+  var instances = M.Datepicker.init(elems, options);
+});
+document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems);
 });
