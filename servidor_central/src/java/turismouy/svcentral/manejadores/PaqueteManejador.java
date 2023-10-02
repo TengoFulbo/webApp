c0dbad12f37594d23453ee0445fc5a209dd7907a
Ezequiel <63671478+Dargon227@users.noreply.github.com>
2023-10-01 23:11:41 -0300

    fix de pool

--- servidor_central/src/java/turismouy/svcentral/manejadores/PaqueteManejador.java
+++ servidor_central/src/java/turismouy/svcentral/manejadores/PaqueteManejador.java
@@ -10,7 +10,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 
 import turismouy.svcentral.EMFactory;
-import turismouy.svcentral.entidades.actividad;
 import turismouy.svcentral.entidades.paquete;
 import turismouy.svcentral.utilidades.log;
 
@@ -88,7 +87,8 @@ public class PaqueteManejador {
         } catch (Exception e) {
             paquete = null;
         }
-
+        
+        em.close();
         return paquete;
     }
 
