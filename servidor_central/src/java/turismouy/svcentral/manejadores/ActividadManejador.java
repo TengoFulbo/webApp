55fc65b3e0e4f1a4b7bc4d6ee8f8d1885aa44124
Ezequiel <63671478+Dargon227@users.noreply.github.com>
2023-10-01 23:25:04 -0300

    parte de fix 😎

--- servidor_central/src/java/turismouy/svcentral/manejadores/ActividadManejador.java
+++ servidor_central/src/java/turismouy/svcentral/manejadores/ActividadManejador.java
@@ -5,13 +5,16 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
 
+import turismouy.svcentral.EMFactory;
 import turismouy.svcentral.entidades.actividad;
 import turismouy.svcentral.utilidades.log;
 
 public class ActividadManejador {
     private static ActividadManejador instancia = null;
     
-    EntityManagerFactory factory = Persistence.createEntityManagerFactory("PA2023");
+    // EntityManagerFactory factory = Persistence.createEntityManagerFactory("PA2023");
+    EntityManagerFactory factory = EMFactory.getEntityManagerFactory();
+
 
     private ActividadManejador(){};
     
@@ -62,7 +65,7 @@ public class ActividadManejador {
 
             tx.commit();       
             log.info("La actividad " + actividad.getNombre() + " se actualizó correctamente");
-            log.info("Salidas: " + actividad.getSalidas().size());
+            // log.info("Salidas: " + actividad.getSalidas().size());
 
         } catch (Exception e) {
             if (tx != null && tx.isActive()) {
