ab2e8250becda0cc1c00d56fc787ff59c83f1d2d
Joaquin Sellanes <135662269+JoaquinSellanes@users.noreply.github.com>
2023-10-17 07:15:13 -0300

    listar actividades

--- servidor_web/src/main/java/turismouy/webapp/home/HomeActividades.java
+++ servidor_web/src/main/java/turismouy/webapp/home/HomeActividades.java
@@ -1,6 +1,8 @@
 package turismouy.webapp.home;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -9,13 +11,23 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/homeActividades")
+import turismouy.svcentral.interfaces.IActividadController;
+import turismouy.svcentral.Fabrica;
+import turismouy.svcentral.datatypes.dataActividad;
+import turismouy.svcentral.utilidades.log;
+
+@WebServlet("/homeActividad")
 public class HomeActividades extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         request.setAttribute("pageTitle", "Actividades - TurismoUY");
 
+        IActividadController IAC = Fabrica.getInstance().getIActividadController();
 
+        List<dataActividad> actividadesList = IAC.getAllActividades();
+
+        request.setAttribute("actividades", actividadesList);
+        log.info("Se accedio a actividades");
         // Redireciona
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/homeActividades.jsp");
         dispatcher.forward(request, response);
