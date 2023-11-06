0402804efe6ca49e5c755f5b29ba459835e05b37
Ezequiel <63671478+Dargon227@users.noreply.github.com>
2023-11-06 05:04:39 -0300

    tema de compra

--- servidor_central/src/java/turismouy/svcentral/controladores/CompraController.java
+++ servidor_central/src/java/turismouy/svcentral/controladores/CompraController.java
@@ -10,10 +10,13 @@ import turismouy.svcentral.entidades.compra_cupo;
 import turismouy.svcentral.entidades.paquete;
 import turismouy.svcentral.entidades.turista;
 import turismouy.svcentral.entidades.usuario;
+import turismouy.svcentral.excepciones.NoExisteExcepcion;
 import turismouy.svcentral.excepciones.ParametrosInvalidosExcepcion;
 import turismouy.svcentral.excepciones.UsuarioNoExisteExcepcion;
 import turismouy.svcentral.excepciones.UsuarioYaExisteExcepcion;
+import turismouy.svcentral.excepciones.YaExisteExcepcion;
 import turismouy.svcentral.interfaces.ICompraController;
+import turismouy.svcentral.manejadores.ActividadManejador;
 import turismouy.svcentral.manejadores.CompraCupoManejador;
 import turismouy.svcentral.manejadores.CompraManejador;
 import turismouy.svcentral.manejadores.PaqueteManejador;
@@ -22,6 +25,72 @@ import turismouy.svcentral.utilidades.log;
 
 public class CompraController implements ICompraController{
 	
+	public void crearCompraV2(String nombreTurista, String nombreActividad, String nombrePaquete, int cantidad) throws ParametrosInvalidosExcepcion, YaExisteExcepcion, NoExisteExcepcion {
+		Boolean debug = true; String debugMsg = "[crearCompraV2] ";
+		UsuarioManejador 		um = UsuarioManejador.getinstance();
+		ActividadManejador 		am = ActividadManejador.getinstance();
+		PaqueteManejador    	pm = PaqueteManejador.getinstance();
+		CompraManejador			cm = CompraManejador.getinstance();
+		CompraCupoManejador 	ccm = CompraCupoManejador.getinstance();
+		int costo = 0;
+
+		paquete paquete = pm.getPaquete(nombrePaquete);
+
+		if (paquete == null) {
+			throw new NoExisteExcepcion("El paquete '" + nombrePaquete + "' no existe.");
+		}
+		if (debug) log.info(debugMsg + "Paquete ok");
+
+		actividad actividad = am.getActividad(nombreActividad);
+
+		if (actividad == null) {
+			throw new NoExisteExcepcion("La actividad '" + nombreActividad + "' no existe.");
+		}
+		if (debug) log.info(debugMsg + "Actividad ok");
+
+		turista turista = (turista) um.getUsuario(nombreTurista);
+
+		if (turista == null) {
+			throw new NoExisteExcepcion("El turista '" + nombreTurista + "' no existe.");
+		}
+		if (debug) log.info(debugMsg + "Turista ok");
+
+		if (pm.TuristaYaComproPaquete(turista, paquete)) {
+			throw new YaExisteExcepcion("El usuario '" + nombreTurista + "' ya tiene el paquete '" + nombrePaquete + "' comprado.");
+		}
+		LocalDate fechaActual = LocalDate.now();
+		LocalDate fechaVencimiento = fechaActual.plusDays(paquete.getValidez());
+
+		log.info("Empezamos con las actividades del paquete.");
+		for (actividad act : paquete.getActividades()) {
+			costo += act.getCosteUni() * cantidad;
+			log.info("	Actividad: " + act.getNombre() + ". Costo uni: '" + act.getCosteUni() + "' Costo: " + costo);
+		}
+
+		compra compra = new compra(LocalDate.now(), cantidad, 2, fechaVencimiento);
+
+		compra.setPaquete(paquete);
+		compra.setTurista(turista);
+
+		turista.addCompra(compra);
+		paquete.addCompra(compra);
+
+		pm.updatePaquete(paquete);
+		um.updateUsuario(turista);
+
+		cm.addCompra(compra);
+
+		List<compra_cupo> cupos = new ArrayList<compra_cupo>();
+		compra compraSaved = cm.getCompra(compra.getId());
+
+		for (actividad act : paquete.getActividades()) {
+			compra_cupo cupo = new compra_cupo(act, cantidad);
+			cupo.setCompra(compraSaved);
+			cupos.add(cupo);
+			ccm.addCupo(cupo);
+		}
+	}
+
 	public void crearCompra (LocalDate fecha, int cantTotal, int costoTotal, LocalDate vencimiento, String nombrePaquete, String nombreTurista)throws ParametrosInvalidosExcepcion, UsuarioYaExisteExcepcion, UsuarioNoExisteExcepcion{
 		
 		PaqueteManejador pm = PaqueteManejador.getinstance();
--- servidor_central/src/java/turismouy/svcentral/interfaces/ICompraController.java
+++ servidor_central/src/java/turismouy/svcentral/interfaces/ICompraController.java
@@ -2,10 +2,14 @@ package turismouy.svcentral.interfaces;
 
 import java.time.LocalDate;
 
+import turismouy.svcentral.excepciones.NoExisteExcepcion;
 import turismouy.svcentral.excepciones.ParametrosInvalidosExcepcion;
 import turismouy.svcentral.excepciones.UsuarioNoExisteExcepcion;
 import turismouy.svcentral.excepciones.UsuarioYaExisteExcepcion;
+import turismouy.svcentral.excepciones.YaExisteExcepcion;
 
 public interface ICompraController {
 	public abstract void crearCompra(LocalDate fecha, int cantTotal, int costoTotal, LocalDate vencimiento, String nombrePaquete, String nombreTurista)throws ParametrosInvalidosExcepcion, UsuarioYaExisteExcepcion, UsuarioNoExisteExcepcion;
+	public abstract void crearCompraV2(String nombreTurista, String nombreActividad, String nombrePaquete, int cantidad) throws ParametrosInvalidosExcepcion, YaExisteExcepcion, NoExisteExcepcion;
+	// public abstract void crearCompra(int cantidad, String nombrePaquete, String nombreTurista);
 }
