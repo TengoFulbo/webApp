e5dc3ef3cdcb10b1179fcc9cec6f340bee950581
Ezequiel <63671478+Dargon227@users.noreply.github.com>
2023-11-27 02:07:35 -0300

    a

---
+++ servidor_web/src/main/java/turismouy/webapp/consultas/ConsultaActividadServlet.java
@@ -0,0 +1,49 @@
+package turismouy.webapp.consultas;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import turismouy.svcentral.middlewares.DataActividad;
+import turismouy.svcentral.middlewares.Publicador;
+import turismouy.svcentral.middlewares.PublicadorService;
+import turismouy.svcentral.utilidades.log;
+
+@WebServlet("/consultaActividad")
+public class ConsultaActividadServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        boolean debug = false;
+        if (debug) log.info("Se accedio a Consulta de actividad");
+
+        String nombreActividad = request.getParameter("nombreActividad");
+        
+        if (debug) log.info("Recibiendo el nombre: " + nombreActividad);
+
+        // Llamamos al web service.
+        Publicador API = new PublicadorService().getPublicadorPort();
+
+        DataActividad actividad = API.actividadGetActividad(nombreActividad);
+
+        if (debug) log.info("-------------------------------");
+        if (debug) log.info("Actividad: " + actividad.getNombre());
+        if (debug) log.info("Descripcion: " + actividad.getDesc());
+        if (debug) log.info("Departamento: " + actividad.getDepartamento().getNombre());
+        if (debug) log.info("FechaCrea: " + actividad.getFechaCrea());
+        if (debug) log.info("-------------------------------");
+
+        // actividad.getDtSalidas().get(1).getNombre();
+
+        // Titulo de la web
+        request.setAttribute("pageTitle", "Consulta de Actividad - TurismoUY");
+
+        request.setAttribute("actividad", actividad);
+
+        // Redireciona
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/consultas/consultaActividad.jsp");
+        dispatcher.forward(request, response);
+    }
+}
---
+++ servidor_web/src/main/java/turismouy/webapp/home/HomePaquetes.java
@@ -0,0 +1,46 @@
+package turismouy.webapp.home;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import turismouy.svcentral.middlewares.DataPaquete;
+import turismouy.svcentral.middlewares.Publicador;
+import turismouy.svcentral.middlewares.PublicadorService;
+import turismouy.svcentral.utilidades.log;
+import turismouy.webapp.utils.LocalDateAdapter;
+
+import java.util.List;
+
+@WebServlet("/homePaquetes")
+public class HomePaquetes extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("pageTitle", "Paquetes - TurismoUY");
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/homePaquetes.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
+        
+        log.info("[HomePaquetes Post]");
+
+        Publicador API = new PublicadorService().getPublicadorPort();
+
+        List<DataPaquete> paquetes = API.paqueteGetAllPaquetes();
+
+        request.setCharacterEncoding("UTF-8");
+        String resultadoJson = gson.toJson(paquetes);
+        response.setContentType("application/json");
+        response.getWriter().write(resultadoJson);
+    }
+}
